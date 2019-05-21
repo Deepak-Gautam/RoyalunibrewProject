8342a8ce36b8096f772e602c8c9d431e038cc378
Yogesh Bhattarai <yoge0030@edu.easj.dk>
2019-05-21 11:24:59 +0200

    jmhj
    mjmhjm

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SvgIconsComponent } from './svg-icons.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('SvgIconsComponent', () => {
-  let component: SvgIconsComponent;
-  let fixture: ComponentFixture<SvgIconsComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SvgIconsComponent ]
+      declarations: [ NavbarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SvgIconsComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
