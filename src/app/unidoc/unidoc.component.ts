8934a82af41533872562f4aca8022fb4e1600d03
Om Bikram Rana <ome.bikram@gmail.com>
2019-06-14 00:55:52 +0200

    Video_added

--- src/app/unidoc/unidoc.component.ts
+++ src/app/unidoc/unidoc.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-unidoc',
   templateUrl: './unidoc.component.html',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnidocComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
